68dc3b4860521fa788c853d82be123b5d9549b3c
cadaverine <kennville@gmail.com>
2019-05-30 01:59:07 +0300

    fix(client): изменена реализация, теперь файл передается фиксированными частями

--- client/main.go
+++ client/main.go
@@ -2,32 +2,58 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net"
+	"path"
 )
 
-func main() {
-	connection, err := net.Dial("tcp", "127.0.0.1:8081")
+func checkError(err error) {
 	if err != nil {
-		panic(err.Error())
+		log.Fatal(err)
 	}
+}
+
+func handleError(value interface{}, err error) interface{} {
+	checkError(err)
+	return value
+}
+
+func main() {
+	connection := handleError(net.Dial("tcp", "127.0.0.1:8081")).(net.Conn)
 	defer connection.Close()
 
 	for {
-		filePath := ""
+		var filePath string
 
 		fmt.Print("Set the path of file to be send: ")
 		fmt.Scanln(&filePath)
 
-		binary, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			panic(err.Error())
-		}
+		fileName := path.Base(filePath)
+		fileNameBytes := []byte(fileName)
+		fileNameBytesLen := int64(len(fileNameBytes))
 
-		fmt.Println("Send file to server: ", filePath)
+		fileBytes := handleError(ioutil.ReadFile(filePath)).([]byte)
+		fileBytesLen := int64(len(fileBytes))
 
-		io.Copy(connection, bytes.NewReader(binary))
+		fmt.Println("Send file to server: ", fileName)
+
+		nameBuf := make([]byte, 8)
+		binary.PutVarint(nameBuf, fileNameBytesLen)
+
+		sizeBuf := make([]byte, 8)
+		binary.PutVarint(sizeBuf, fileBytesLen)
+
+		// длина названия файла
+		io.CopyN(connection, bytes.NewReader(nameBuf), 8)
+		// байты названия файла
+		io.CopyN(connection, bytes.NewReader(fileNameBytes), fileNameBytesLen)
+		// размер файла
+		io.CopyN(connection, bytes.NewReader(sizeBuf), 8)
+		// байты файла
+		io.CopyN(connection, bytes.NewReader(fileBytes), fileBytesLen)
 	}
 }
